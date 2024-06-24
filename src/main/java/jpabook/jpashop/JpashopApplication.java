62d26b976a7a547ee7be8d4dedfc4a7eb3cd8270
hcghcg17 <hcghcg17@naver.com>
2024-06-24 22:55:15 +0900

    Feat : JPA or DB 설정완료

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,13 +1,24 @@
 package jpabook.jpashop;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JpashopApplication {
-
+	
+	@Autowired
+	MemberRepository memberRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JpashopApplication.class, args);
+
+//		서비스에서 호출
+//		Member member = new Member();
+//		member.setUsername("memeberA");
+//		Long saveId = memberRepository.save(member);
+//		Member findMember = memberRepository.find(saveId);
+//		System.out.println(findMember.toString());
 	}
 
 }
