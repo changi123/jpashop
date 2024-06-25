8cfdfb971c3c8d9d22894c90e53eabd44627f3bb
hcghcg17 <hcghcg17@naver.com>
2024-06-25 16:36:41 +0900

    Feat : 엔티티, 테이블 작성완료

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -9,5 +9,19 @@ import lombok.Getter;
 public class Address {
 	private String city; 
 	private String street; 
-	private String zipcode; 
+	private String zipcode;
+	
+	
+	protected Address() {
+		
+	}
+	
+	
+	public Address(String city, String street, String zipcode) {
+		this.city = city;
+		this.street = street;
+		this.zipcode = zipcode;
+	} 
+	
+	
 }
