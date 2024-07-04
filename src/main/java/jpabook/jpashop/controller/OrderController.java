559c9f354b50cdf47d54562ab0ef427a08fcb0d4
hcghcg17 <hcghcg17@naver.com>
2024-07-04 17:24:13 +0900

    Feat : OrderController 수정

--- src/main/java/jpabook/jpashop/controller/OrderController.java
+++ src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -5,16 +5,24 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.OrderSearch;
 import jpabook.jpashop.service.ItemService;
 import jpabook.jpashop.service.MemberService;
 import jpabook.jpashop.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class OrderController {
 	private final OrderService orderService;
 	private final MemberService memberService;
@@ -31,4 +39,28 @@ public class OrderController {
 		
 		return "order/orderForm";
 	}
+	
+	@PostMapping("/order")
+	public String order(@RequestParam("memberId") Long memberId,
+			@RequestParam("itemId") Long itemId,
+			@RequestParam("count") int count
+			) {
+		orderService.order(memberId, itemId, count);
+		
+		return "redirect:/orders";
+	}
+	
+    @GetMapping("/orders")
+	public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch , Model model) {
+		List<Order> orders = orderService.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+        
+		return "order/orderList";
+	}
+    
+    @PostMapping("/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable("orderId") Long orderId) {
+        orderService.cancelOrder(orderId);
+        return "redirect:/orders";
+    }
 }
