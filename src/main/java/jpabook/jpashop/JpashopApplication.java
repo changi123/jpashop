9a9de2370f781a58bdfb2fbd0607b02656547427
hcghcg17 <hcghcg17@naver.com>
2024-09-02 20:54:50 +0900

    Feat : Hibernate5JakartaModule 빈 등록

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,8 +1,10 @@
 package jpabook.jpashop;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 
 import jpabook.jpashop.repository.MemberRepository;
 
@@ -10,7 +12,18 @@ import jpabook.jpashop.repository.MemberRepository;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(JpashopApplication.class, args);
+		
 	}
+
+
+	
+	   @Bean
+	   Hibernate5JakartaModule hibernate5Module() {
+		   Hibernate5JakartaModule  hibernate5JakartaModule = new Hibernate5JakartaModule();
+		   hibernate5JakartaModule.configure(Hibernate5JakartaModule.Feature.FORCE_LAZY_LOADING, true);
+		   return hibernate5JakartaModule;
+	   }
 
 }
